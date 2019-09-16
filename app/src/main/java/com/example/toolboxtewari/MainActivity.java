49cbeffa733238112ca3c1e59ccbb96d97f3ff2f
YeeterSkeeter <tewarisuyash@gmail.com>
2019-09-15 22:57:32 -0500

    added bread choice and sandwich choice

--- app/src/main/java/com/example/toolboxtewari/MainActivity.java
+++ app/src/main/java/com/example/toolboxtewari/MainActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
         import android.os.Bundle;
         import android.view.View;
-        import android.widget.ImageView;
-        import android.widget.Toast;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     ImageView subwayImage;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         subwayImage = (ImageView) findViewById(R.id.subwayImage);
+        spinner = (Spinner) findViewById(R.id.sandwichChoices);
 
         subwayImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +30,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        String[] sandwichChoices = new String[]{
+                "Ham", "Oven Roasted Chicken", "Sweet Onion Chicken Teriyaki", "Veggie Delite"
+        };
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, R.layout.spinner_item, sandwichChoices);
+
+        adapter.setDropDownViewResource(R.layout.spinner_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String text = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(adapterView.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
